04480e4023005b2f2e921d2b5aaee0b866d52b85
Mitchell Cheng <chengbotier1@gmail.com>
2022-11-28 20:22:31 +0800

    add sorting algo

--- sorting/sorting.js
+++ sorting/sorting.js
@@ -1,4 +1,5 @@
-// implement bubble sort using javascript
+/**
+ * bubble sort
 const bubbleSort = function (arr) {
 	for (let i = 0; i < arr.length; i += 1) {
 		for (let j = i; j < arr.length; j += 1) {
@@ -10,6 +11,73 @@ const bubbleSort = function (arr) {
 		}
 	}
 }
+*/
+
+/**
+ * selection sort
+const selectionSort = function (arr) {
+	for (let i = 0; i < arr.length; i += 1) {
+		let min = i;
+		for (let j = i; j < arr.length; j += 1) {
+			if (arr[j] < arr[min]) min = j;
+		}
+		let temp = arr[i];
+		arr[i] = arr[min];
+		arr[min] = temp;
+	}
+}
+*/
+
+/**
+ * count sort
+const countSort = function (arr) {
+	const max = Math.max(...arr);
+	let bucket = new Array(max + 1);
+	for (let i = 0; i < bucket.length; i += 1) {
+		bucket[i] = 0;
+	}
+	for (let i = 0; i < arr.length; i += 1) {
+		bucket[arr[i]] += 1;
+	}
+	let i = 0;
+	let sortedArr = [];
+	while (i < bucket.length) {
+		if (bucket[i] > 0) {
+			sortedArr.push(i);
+			bucket[i] -= 1;
+		} else i += 1;
+	}
+	return sortedArr;
+}
+*/
+
+
+/** 
+ * merge
+const merge = function (arr1, arr2) {
+	let mergedArr = [];
+	let i = 0;
+	let j = 0;
+	while (i < arr1.length || j < arr2.length) {
+		if (arr1[i] < arr2[j]) {
+			mergedArr.push(arr1[i]);
+			i += 1;
+		} else {
+			mergedArr.push(arr2[j]);
+			j += 1;
+		}
+	}
+	while (i < arr1.length) {
+		mergedArr.push(arr1[i]);
+		i += 1;
+	}
+	while (j < arr2.length) {
+		mergedArr.push(arr2[j]);
+		j += 1;
+	}
+	return mergedArr;
+}
+*/
 
 // test code
 const print = function (arr) {
@@ -18,9 +86,10 @@ const print = function (arr) {
 	});
 }
 
-array = [3, 6, 2, 4, 1, 0, 5];
+array = [33, 65, 21, 14, 11, 0, 5];
+//array2 = [1,9,6,4,2,0];
+//console.log(merge(array, array2));
 console.log("original array:");
 print(array);
-bubbleSort(array);
 console.log("sorted array:");
-print(array);
+print(arr);
--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,6 +1,7 @@
+"""
 def bubble_sort(arr):
 	'''
-	implement bubble sort using python
+	bubble sort
 	'''
 	for i in range(len(arr)):
 		for j in range(i, len(arr)):
@@ -8,16 +9,51 @@ def bubble_sort(arr):
 				temp = arr[i]
 				arr[i] = arr[j]
 				arr[j] = temp
+"""
+
+"""
+def insertion_sort(arr):
+	'''
+	insertion sort
+	'''
+	for i in range(len(arr)):
+		min = i 
+		for j in range(i + 1, len(arr)):
+			if arr[j] < arr[min]:
+				min = j
+		temp = arr[i]
+		arr[i] = arr[min]
+		arr[min] = temp
+"""
+
+"""
+def count_sort(arr):
+	'''
+	count sort
+	'''
+	max_value = max(arr)
+	bucket = [0] * (max_value + 1)
+	for i in range(len(bucket)):
+		bucket[arr[i]] += 1
+	sorted_arr = list()
+	i = 0
+	while i < len(bucket):
+		if bucket[i] > 0:
+			sorted_arr.append(i)
+			bucket[i] -= 1
+		else:
+			i += 1
+	return sorted_arr
+"""
+
 
 def print_array(arr):
 	for i in arr:
 		print(i)
 
-
 # test code
-array = [3, 6, 2, 4, 1, 0, 5]
+array = [33, 16, 22, 42, 12, 1, 5]
 print("origin array:")
 print_array(array)
-bubble_sort(array)
 print("sorted array:")
-print_array(array)
+print_array(arr)
