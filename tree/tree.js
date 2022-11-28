b646eaaa089c574355dae5fcf84f861e0fe29bc3
Mitchell Cheng <chengbotier1@gmail.com>
2022-11-28 12:18:57 +0800

    implement tree

---
+++ tree/tree.js
@@ -0,0 +1,87 @@
+/*
+// implement linkedlist-based tree using ES6 class syntax
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class MyTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  preorderTraversal(root = this.root) {
+    if (root === null) return;
+    console.log(root.value);
+    this.preorderTraversal(root.left);
+    this.preorderTraversal(root.right);
+  }
+
+  inorderTraversal(root = this.root) {
+    if (root === null) return;
+    this.inorderTraversal(root.left);
+    console.log(root.value);
+    this.inorderTraversal(root.right);
+  }
+
+  postorderTraversal(root = this.root) {
+    if (root === null) return;
+    this.postorderTraversal(root.left);
+    this.postorderTraversal(root.right);
+    console.log(root.value);
+  }
+}
+*/
+
+// implement linkedlist-based tree using vanilla js
+const Node = function (value, left = null, right = null) {
+  this.value = value;
+  this.left = left;
+  this.right = right;
+
+}
+
+const MyTree = function (root = null) {
+  this.root = root;
+
+  this.preorderTraversal = function (root = this.root) {
+    if (root === null) return;
+    console.log(root.value);
+    this.preorderTraversal(root.left);
+    this.preorderTraversal(root.right);
+  }
+
+  this.inorderTraversal = function (root = this.root) {
+    if (root === null) return;
+    this.inorderTraversal(root.left);
+    console.log(root.value);
+    this.inorderTraversal(root.right);
+  }
+
+  this.postorderTraversal = function (root = this.root) {
+    if (root === null) return;
+    this.postorderTraversal(root.left);
+    this.postorderTraversal(root.right);
+    console.log(root.value);
+  }
+}
+
+
+// test code
+let root = new Node(1);
+let leftChild = new Node(2);
+let rightChild = new Node(3);
+root.left = leftChild;
+root.right = rightChild;
+let tree = new MyTree(root);
+console.log('this tree is now looks like:');
+console.log(tree);
+console.log('preorder traversal:');
+tree.preorderTraversal();
+console.log('inorder traversal:');
+tree.inorderTraversal();
+console.log('postorder traversal:');
+tree.postorderTraversal();
---
+++ tree/tree.py
@@ -0,0 +1,50 @@
+class Node:
+    """
+    implement tree node
+    """
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class MyTree:
+    """
+    implement linkedlist-based tree
+    """
+    def __init__(self, root=None):
+        self.root = root
+
+    def preorder_traversal(self, root=None):
+        if root == None:
+            return
+        print(root.value)
+        self.preorder_traversal(root.left)
+        self.preorder_traversal(root.right)
+
+    def inorder_traversal(self, root=None):
+        if root == None:
+            return
+        self.inorder_traversal(root.left)
+        print(root.value)
+        self.inorder_traversal(root.right)
+
+    def postorder_traversal(self, root=None):
+        if root == None:
+            return
+        self.postorder_traversal(root.left)
+        self.postorder_traversal(root.right)
+        print(root.value)
+
+# test code
+root = Node(1)
+left_child = Node(2)
+right_child = Node(3)
+root.left = left_child
+root.right = right_child
+tree = MyTree(root)
+print("preorder traversal:")
+tree.preorder_traversal(root)
+print("inorder traversal:")
+tree.inorder_traversal(root)
+print("postorder traversal:")
+tree.postorder_traversal(root)
